f45091fd9c68dcf726829c774108a24ae76611be
Alex Dasneves <adasneves127@gmail.com>
2023-12-24 00:19:15 -0500

    Finished add and edit

--- app.py
+++ app.py
@@ -16,11 +16,6 @@ def get_root():
         return "Records File not found! Please contact your systems administrator", 500
     return render_template("main.liquid", records=items)
 
-
-@app.route("/new")
-def new_item():
-    return ""
-
 @app.route("/view/")
 def return_error_no_view():
     return "Err: Invoice ID not supplied", 400
@@ -47,4 +42,56 @@ def get_image():
     
 @app.route('/new/')
 def create_inv():
+    
     return render_template("new_invoice.liquid")
+
+
+@app.post("/new/")
+def create_inv_post():
+    try:
+        items = json.load(open("records.json"))
+    except FileNotFoundError:
+        return "Records File not found! Please contact your systems administrator", 500
+    # get the data from the json body
+    data = request.get_json()
+    # add the data to the items list
+    items.append(data)
+    # write the data to the file
+    json.dump(items, open("records.json", "w"), indent=4)
+    # return the ID
+    return "Record created with ID: {}".format(data.get("id")), 201
+
+@app.route("/edit/<ID>")
+def edit_inv(ID=None):
+    if ID is None:
+        return "Invoice ID not supplied", 400
+    try:
+        items = json.load(open("records.json"))
+    except FileNotFoundError:
+        return "Records File not found! Please contact your systems administrator", 500
+    
+    for item in items:
+        if ID == item.get("id"):
+            return render_template("edit_invoice.liquid", **item)
+    
+    return "Item not found!", 404
+
+@app.patch("/edit/<ID>")
+def edit_inv_post(ID=None):
+    if ID is None:
+        return "Invoice ID not supplied", 400
+    try:
+        items = json.load(open("records.json"))
+    except FileNotFoundError:
+        return "Records File not found! Please contact your systems administrator", 500
+    # get the data from the json body
+    data = request.get_json()
+    # add the data to the items list
+    for item in items:
+        if ID == item.get("id"):
+            for key, value in data.items():
+                item[key] = value
+    # write the data to the file
+    json.dump(items, open("records.json", "w"), indent=4)
+    # return the ID
+    return "Record edited with ID: {}".format(data.get("id")), 201
